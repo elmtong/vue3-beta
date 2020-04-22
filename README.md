7d889b34fd1326372eb2595311b35ee7cee1a386
MOTONGTONG <36754785+MOTONGTONG@users.noreply.github.com>
2020-04-22 18:42:36 +0800

    init初始化代码

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # vue3-beta
-:swimmer:vue3公测版尝尝鲜
+
+## Project setup
+
+```
+npm install or cnpm  install
+```
+
+### Compiles and hot-reloads for development
+
+```
+npm run serve
+```
+
+### Compiles and minifies for production
+
+```
+npm run build
+```
+
+### Lints and fixes files
+
+```
+npm run lint
+```
+
+### references
+
+[Vue Composition API](https://vue-composition-api-rfc.netlify.app/api.html)
---
+++ src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <h1>{{title.name}}</h1>
+    <div>{{user}}</div>
+    <button @click="updateUser">修改名称</button>
+    <div>当前count：{{computedCount}}</div>
+    <button @click="increment">count + 1</button>
+  </div>
+  <div>
+    <span>不再局限于模板中的单个根节点</span>
+  </div>
+</template>
+
+<script>
+import { reactive,ref,onMounted,computed } from 'vue'
+export default {
+  name: 'App',
+  setup(){
+    // 变量
+    const title = reactive({
+      name:'welcome to vue 3 beta!'
+    })
+    const user = ref('ref黑铁');
+
+    const updateUser = () => {
+      user.value = 'ref青铜'
+    }
+
+    // 钩子函数
+    onMounted(() => {
+      console.log('俺是mounted钩子！')
+    })
+
+    // 初始化count值
+    const count = ref(0);
+
+    // 事件处理
+    const increment = () => {
+      count.value++
+    }
+
+    // 调用计算属性函数Hook
+    const computedCount = computed(() => count.value*100)
+
+    return { title , user, updateUser, count, increment, computedCount }
+  }
+}
+</script>
+
+<style>
+#app {
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
